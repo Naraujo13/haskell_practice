a8034667108af1b3aa8fb849800dc6fc76e37cc9
Nícolas Oreques de Araujo <nicolas.oreques@gmail.com>
2018-05-15 18:24:37 -0300

    Adicionadas funções para justificar linha e justificar texto (com problemas)

--- trabalho1.hs
+++ trabalho1.hs
@@ -4,20 +4,27 @@
 ----- Justificar um texto segundo maior linha -----
 ---------- Última linha não justifica -------------
 
--- justifica :: String -> String
--- justifica [] = []
--- justifica x = (separaLinhas x)
+texto_exemplo = "RUBIÃO fitava a enseada -- eram oito horas da manhã.\nQuem o visse com os polegares metidos no cordão do chambre à janela de uma\ngrande casa de Botafogo cuidaria que ele admirava aquele pedaço de água\nquieta mas em verdade vos digo que pensava em outra coisa.\nCotejava o passado com o presente. Que era há um ano?\nProfessor. Que é agora? Capitalista! Olha para si para as chinelas\n(umas chinelas de Túnis que lhe deu recente amigo Cristiano Palha) para a casa\npara o jardim para a enseada para os morros e para o céu e tudo desde as chinelas\naté o céu tudo entra na mesma sensação de propriedade.\n"
 
+justifica :: String -> String
+justifica [] = []
+justifica x =
+  let
+    array_linhas = (separaLinhas x)
+  in
+    (justificaLinhas (init array_linhas) (tamanhoMaiorLinha array_linhas)) ++ (last(array_linhas)) ++ "\n"
+
+--- [Lista de Linhas] -> TamanhoMaiorLinha -> TextoJustificado
 -- --- Justifica lista de linhas ---
--- justificaLinhas :: [String] -> [String]
--- justificaLinhas [] = []
--- justificaLinhas (x:xs) = justificaLinha x : justificaLinhas xs
+justificaLinhas :: [String] -> Int -> String
+justificaLinhas [] n = []
+justificaLinhas (x:xs) n = (justificaLinha x n) ++ "\n" ++ (justificaLinhas xs n)
 
 --- Justifica uma linha ---
 --- Linha -> Tamanho Alvo -> Linha Justificada ---
 justificaLinha :: String -> Int -> String
 justificaLinha [] n = []
-justificaLinha x n = insereEspacos (fst(numeroEspacosPorPalavra x n)) x
+justificaLinha x n = insereEspacos (fst(numeroEspacosPorPalavra x ( n - (length x)))) x
 
 --- Separa o texto por linhas ---
 separaLinhas :: String -> [String]
@@ -32,7 +39,14 @@ tamanhoMaiorLinha (x:xs) = max (length x) (tamanhoMaiorLinha xs)
 --- Separa o texto em palavras ---
 separaPalavras :: String -> [String]
 separaPalavras [] = []
-separaPalavras x = (takeWhile (/=' ') x) : separaPalavras (tail (dropWhile (/=' ') x))
+separaPalavras x =
+  let
+      y = (dropWhile(/=' ') x)
+      notEmpty
+        | (y /= []) = (takeWhile (/=' ') x) : separaPalavras (tail (dropWhile (/=' ') x))
+        | otherwise = (takeWhile (/=' ') x) : []
+  in
+      notEmpty
 
 --- Número de Palavras ---
 numeroPalavras :: String -> Int
@@ -40,7 +54,7 @@ numeroPalavras [] = 0
 numeroPalavras x = length (separaPalavras x)
 
 --- Calcula Número de Espaços a ser inserido entre palavras ---
---- Texto -> TamanhoAlvo -> Numero Espacos por palavra
+--- Texto -> Numero de Espaços para inserir -> Numero Espacos por palavra
 numeroEspacosPorPalavra :: String -> Int -> (Int, Int)
 numeroEspacosPorPalavra [] n = (0, 0)
 numeroEspacosPorPalavra x n =  (div n (numeroPalavras x - 1), rem n (numeroPalavras x - 1))
